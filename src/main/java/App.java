c1885b4512597c839640f2e7c3d84b9848c52d62
Gurovij <gurovij@bk.ru>
2020-05-08 10:26:05 +0300

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = 
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(bean2));
+        Cat catbean = (Cat) applicationContext.getBean("cat");
+        Cat catbean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean.equals(catbean2));
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+
+    Cat(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
